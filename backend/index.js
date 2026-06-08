6a5f95e082b0bf65f5b608d94a62ec0c1877069d
Oluwakemi Adeosun <oluwakemilola02@gmail.com>
2026-06-08 08:45:10 +0100

    Add health check endpoint for Survey API

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/survey", surveyRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).json({ 
+    message: "SachaPay Survey API is running" 
+  });
+});
+
 connectDB();
 
 app.listen(PORT, () => {
